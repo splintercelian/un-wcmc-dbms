9f0bf92c7349fbb647b995a724860fbe938a172a
splintercelian <splintercelian@hotmail.com>
2023-12-06 23:40:14 -0500

    Added menus and menu bar with quit function

--- un-wcmc-dbms.cpp
+++ un-wcmc-dbms.cpp
@@ -29,7 +29,9 @@ bool UnWcmcDbms::OnInit() {
 TabFrame::TabFrame(const wxString titre, int xpos, int ypos, int width, int height):wxFrame((wxFrame *) NULL, -1, titre, wxPoint(xpos, ypos), wxSize(width, height)) {
     //initialisation des pointeurs et variables
     grid = (wxGrid *) NULL;
-    const wxString nomFichier = "C:/Temp/un-wcmc-dbms/data_wcmc.txt";
+    menuBar = (wxMenuBar *) NULL;
+    fichierMenu = (wxMenu *) NULL;
+    const wxString nomFichier;
 
     //creation de l'instance de wxGrid
     grid = new wxGrid(this, -1, wxDefaultPosition, wxDefaultSize);
@@ -37,27 +39,38 @@ TabFrame::TabFrame(const wxString titre, int xpos, int ypos, int width, int heig
     //creation de la grille (la taille sera ajustee dynamiquement)
     grid->CreateGrid(0, 0);
 
-    //chargement des donnees
-    LoadData(nomFichier);
-
-    //fait dodo
-    Sleep(1000);
-
-    //sauvegarde des donnees
-    SaveData(wxT("saveTest.txt"));
+    //creation des menus
+    fichierMenu = new wxMenu;
+    infoMenu = new wxMenu;
+    //affectation des id et libelles au differents menus
+    fichierMenu->Append(ID_OUVRIR, "&Ouvrir fichier");
+    fichierMenu->Append(ID_SAUVEGARDER, "&Sauvergarder fichier");
+    fichierMenu->AppendSeparator();
+    fichierMenu->Append(ID_QUITTER, "&Quitter");
+    infoMenu->Append(ID_APROPOS, "&A propos");
+    //creation de la barre de menu et affectation des menus
+    menuBar = new wxMenuBar;
+    menuBar->Append(fichierMenu, "&Fichier");
+    menuBar->Append(infoMenu, "&Info");
+    //ajout de la barre de menu au cadre
+    SetMenuBar(menuBar);
 }
 
+TabFrame::~TabFrame() {}
+
 void TabFrame::LoadData(const wxString& nomFichier) {
-    //initialisation de variables
+    //declaration et initialisation de variables
     wxTextFile fichier(nomFichier);
     wxString ligne;
     int row = 0;
 
+    //si le fichier ne s'ouvre pas -> envoi d'un message a l'utilisateur
     if(!fichier.Open()) {
         wxMessageBox("Impossible d'ouvrir le fichier", "Erreur", wxOK | wxICON_ERROR);
         return;
     }
     
+    //boucle sur les lignes du fichier (lecture ligne par ligne)
     for(ligne = fichier.GetFirstLine(); !fichier.Eof(); ligne = fichier.GetNextLine()) {
         //separation de la ligne lue en morceaux representant les differents champs de la table
         wxStringTokenizer tokenizer(ligne, "\t", wxTOKEN_RET_EMPTY);
@@ -68,6 +81,7 @@ void TabFrame::LoadData(const wxString& nomFichier) {
         //initialisation d'une variable colonne pour lister les champs recuperes
         int col = 0;
 
+        //tant que des valeurs sont presentes ajout de la valeur suivante dans une nouvelle colonne
         while(tokenizer.HasMoreTokens()) {
             //recuperation de la valeur du champ
             wxString token = tokenizer.GetNextToken();
@@ -75,7 +89,7 @@ void TabFrame::LoadData(const wxString& nomFichier) {
             //remplacement des guillemets par rien du tout
             token.Replace(wxT("\""), wxT(""));
 
-            //ajout d'une colonne dans la grille pour contenir le champ si il s'agit de la premiere ligne
+            //ajout d'une colonne dans la grille pour contenir le champ, si il s'agit de la premiere ligne
             if(row == 0)
                 grid->AppendCols();
 
@@ -95,20 +109,59 @@ void TabFrame::LoadData(const wxString& nomFichier) {
 }
 
 void TabFrame::SaveData(const wxString& nomFichier) {
+    //declaration et initialisation des variables
     int ligne = 0, colonne = 0;
     wxString data = "";
     wxTextFile fichier;
+
+    //creation d'un fichier
     fichier.Create(nomFichier);
-    for(ligne = 0; ligne < grid->GetNumberRows(); ligne++) {
-        for(colonne = 0; colonne < grid->GetNumberCols(); colonne++) {
-            data += "\"" + grid->GetCellValue(ligne, colonne) + "\"";
+    
+    //boucle sur l'ensemble des cellules pour enregistrer les valeurs dans le fichier selon le format attendu
+    for(ligne = 0; ligne < grid->GetNumberRows(); ligne++) { //boucle sur les lignes
+        for(colonne = 0; colonne < grid->GetNumberCols(); colonne++) { //boucle sur les colonnes
+            data += "\"" + grid->GetCellValue(ligne, colonne) + "\""; //ajout des guillemets a la valeur du champ
+            //si il ne s'agit pas de la derniere colonne ajout d'une tabulation
             if(colonne != grid->GetNumberCols() - 1)
                 data += "\t";
         }
-        fichier.AddLine(data);
-        data = "";
+        fichier.AddLine(data); //ajout ligne par ligne dans le fichier
+        data = ""; //re-initialisation de data pour la prochaine ligne
     }
+    //ecriture dans le fichier
     fichier.Write();
 }
 
-TabFrame::~TabFrame() {}
+//implementation de la table pour la gestion des evenements
+BEGIN_EVENT_TABLE(TabFrame, wxFrame)
+    EVT_MENU(ID_OUVRIR, TabFrame::OnFichierOuvrir)
+    EVT_MENU(ID_SAUVEGARDER, TabFrame::OnFichierSauvegarder)
+    EVT_MENU(ID_QUITTER, TabFrame::OnFichierQuitter)
+    EVT_MENU(ID_APROPOS, TabFrame::OnInfoApropos)
+END_EVENT_TABLE()
+
+//implementation des methodes de TabFrame liees aux differents menus
+void TabFrame::OnFichierOuvrir(wxCommandEvent &event) {
+    wxFileDialog *dlg = new wxFileDialog(this, "Ouvrir un fichier texte", "", "", "Tous les fichiers(*.*)|*.*|Fichiers Textes(*.txt)|*.txt", wxFD_OPEN, wxDefaultPosition);
+    if(dlg->ShowModal() == wxID_OK)
+        LoadData(dlg->GetFilename());
+    dlg->Destroy();
+}
+void TabFrame::OnFichierSauvegarder(wxCommandEvent &event) {
+    wxFileDialog *dlg = new wxFileDialog(this, "Sauvegarder un fichier texte", "", "", "Tous les fichiers(*.*)|*.*|Fichiers Textes(*.txt)|*.txt", wxFD_SAVE, wxDefaultPosition);
+    if(dlg->ShowModal() == wxID_OK)
+        SaveData(dlg->GetFilename());
+    dlg->Destroy();
+}
+void TabFrame::OnFichierQuitter(wxCommandEvent &event) {
+    wxMessageDialog *dlg = new wxMessageDialog(this, "Voulez-vous vraiment quitter le programme?", "Boite de dialogue", wxYES_NO | wxNO_DEFAULT | wxCENTRE | wxICON_QUESTION, wxDefaultPosition);
+    if(dlg->ShowModal() == wxID_NO)
+        return;
+    else
+        Close(FALSE);
+}
+void TabFrame::OnInfoApropos(wxCommandEvent &event) {
+    wxMessageDialog *dlg = new wxMessageDialog(this, "Gestionnaire de table de donnees basique", "Message", wxOK | wxCENTRE | wxICON_INFORMATION, wxDefaultPosition);
+    dlg->ShowModal();
+    dlg->Destroy();
+}
--- un-wcmc-dbms.h
+++ un-wcmc-dbms.h
@@ -4,7 +4,6 @@
 #define UN_WCMC_DBMS_H
 
 //declare l'application "un-wcmc-dbms"
-
 class UnWcmcDbms : public wxApp {
     public:
         //methode "OnInit()" qui doit etre redefinie
@@ -14,13 +13,36 @@ class UnWcmcDbms : public wxApp {
 //classe pour la fenetre principale
 class TabFrame : public wxFrame {
     public:
-        TabFrame(const wxString titre, int xpos, int ypos, int width, int height);
-        void LoadData(const wxString& nomFichier);
-        void SaveData(const wxString& nomFichier);
-        ~TabFrame();
+        //declaration des methodes publiques de la classe TabFrame
+        TabFrame(const wxString titre, int xpos, int ypos, int width, int height); //constructeur
+        void LoadData(const wxString& nomFichier); //pour charger les donnees d'un fichier dans le tableur
+        void SaveData(const wxString& nomFichier); //pour sauvegarder les donnees du tableur dans un fichier
+        ~TabFrame(); //destructeur
+        //declaration des menus
+        //menu Fichier | Ouvrir
+        void OnFichierOuvrir(wxCommandEvent &event);
+        //menu Fichier | Sauvegarder
+        void OnFichierSauvegarder(wxCommandEvent &event);
+        //menu Fichier | Quitter
+        void OnFichierQuitter(wxCommandEvent &event);
+        //menu Info | Apropos
+        void OnInfoApropos(wxCommandEvent &event);
     private:
         wxGrid *grid;
         wxFile *file;
+        wxMenuBar *menuBar;
+        wxMenu *fichierMenu;
+        wxMenu *infoMenu;
+        //structure d'enumeration pour les identifiants des menus
+        enum {
+            ID_QUITTER = 100,
+            ID_OUVRIR,
+            ID_SAUVEGARDER,
+            ID_APROPOS
+        };
+    protected:
+        //declaration de la directive pour gerer les evenements via une table d'evenements
+        DECLARE_EVENT_TABLE();
 };
 
 #endif
